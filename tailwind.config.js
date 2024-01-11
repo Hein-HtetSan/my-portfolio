a7a51d3c933104a276b5f0a264c57ac3a7aa6f5a
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-01-12 00:33:18 +0630

    up to date ui design for portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class', , '[data-mode="dark"]'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
