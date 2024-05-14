1e8c0a023ad5a99f604083eb448ca70bcd93f0e2
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-05-14 17:18:56 +0630

    test image

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,25 +2,23 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
-use MongoDB\Laravel\Eloquent\Model as Eloquent;
-use MongoDB\Laravel\Auth\User as AuthenticatableMongoDB;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Auth\User as Eloquent;
 
-class User extends Eloquent implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
+class User extends Eloquent
 {
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
-    use Authenticatable, Authorizable, CanResetPassword;
 
     protected $connection = 'mongodb';
 
