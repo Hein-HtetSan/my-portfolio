429a2900faf69bb9cff69c8a53b9d2b0ff7733bf
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-06-30 16:06:30 +0630

    prepare for project section

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -13,5 +13,29 @@ class LanguageSeeder extends Seeder
     public function run(): void
     {
         //
+        $langs = [
+            ['PHP', 'Laravel', 'SQL'],
+            ['PHP', 'MySQL'],
+            ['J2EE', 'JSP', 'JDBC', 'MySQL'],
+            // ['PHP', 'Laravel', 'MySQL'],
+            ['Python'],
+            ['JavaFx', 'MySQL'],
+            ['J2EE', 'Bootstrap', 'MySQL'],
+            ['Python', 'Django', 'SQL'],
+            ['Express JS', 'GraphQL', 'React JS', 'Electron'],
+            ['PHP', 'Laravel', 'MySQL'],
+            ['CSS', 'JavaScript'],
+            ['Bootstrap', 'JavaScript'],
+            ['Bootstrap', 'JavaScript'],
+            ['PHP', 'MySQL'],
+            ['PHP'],
+            ['CSS', 'JavaScript'],
+            ['HTML', 'CSS', 'JavaScript'],
+            ['Tailwindcss'],
+            ['JavaScript'],
+            ['CSS', 'JavaScript'],
+            ['JavaScript'],
+            ['JavaScript']
+        ];
     }
 }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -20,8 +20,7 @@ class ProjectSeeder extends Seeder
             'CC Shop',
             // 'Bloggy',
             'AI Voice Assistance',
-            'Library Management System',
-            'Digital Hunter Design',
+            'Student Management System',
             'Travel Booking System',
             'Little Lemon',
             'Notedly',
@@ -32,28 +31,85 @@ class ProjectSeeder extends Seeder
             'To Do List',
             'Real Time Chat',
             'Digital Hunter 2nd Challenge',
+            'Digital Hunter B5 Solution',
             'Easy Bank Landing UI',
             'Rating Solution',
             'Space Tourism UI',
-            'Digital Hunter B5 Solution',
             'Bee Music',
             'Music Player'
         ];
 
         $project_source_code = [ // projects' github links
-
+            'pizza-order-system',
+            'book-haven-project',
+            'cc-shop',
+            // 'bloggy',
+            'Ai_voice_Assistance',
+            'student-management-mini-project',
+            'travel-booking-j2ee',
+            'little-lemon',
+            'Notedly',
+            'ConNET',
+            'Movie',
+            'learning-app-project',
+            'audibook-ui-project',
+            'todoList',
+            'RealTime_chat',
+            'DigitalHunderChanllenge2',
+            'DigitalHunterB5Solution',
+            'easybanklandingpage',
+            'RatingSolution',
+            'SpaceTourism',
+            'beeMusic',
+            'musicPlayer'
         ];
 
         $project_short_desc = [ // projects' short description
-
+            'Pizza Order System in CodeLab Project',
+            'Final Project of YHA Computer Training Center',
+            '2nd Year, 2nd Sem Final Project with J2EE',
+            // 'Blog System Project with Laravel',
+            'AI Voice Assistance Project with Python',
+            'Student Management System Enterprise Application with Java, ScenceBuilder',
+            'Travel Booking System web application with J2EE',
+            'Little Lemon - A simple e-commerce website with Django Python',
+            'Notedly - A note taking app with React JS, Express, GraphQL',
+            'ConNET - A simple to do list project with Laravel',
+            'Movie - A simple movie UI with pure css. Just to show, I am professional in CSS',
+            'Learning App - A simple learning site UI with HTML, CSS, JS',
+            'AudiBook - UI Assignment from YHA Computer',
+            'Todo List - A simple todo list project with PHP',
+            'RealTime Chat - A simple chat app with PHP',
+            'Digital Hunter Challenge 2 - UI Project Challenge from DigitalHunter Course',
+            'Digital Hunter Challenge 5 - UI Project Challenge from DigitalHunter Course',
+            'Easy Bank Landing Page - Frontend Mentor Project Challenge',
+            'Rating Solution - UI Project Challenge from Frontend Mentor',
+            'beeMusic - Just to practice the JavaScript, Second Project',
+            'Music Player - Just to practice the JavaScript, First Project'
         ];
 
         $project_content = [// project content
-
-        ];
-
-        $project_demo_link = [ // projects' demo links
-
+            'Pizza Order System in CodeLab Project',
+            'Final Project of YHA Computer Training Center',
+            '2nd Year, 2nd Sem Final Project with J2EE',
+            // 'Blog System Project with Laravel',
+            'AI Voice Assistance Project with Python',
+            'Student Management System Enterprise Application with Java, ScenceBuilder',
+            'Travel Booking System web application with J2EE',
+            'Little Lemon - A simple e-commerce website with Django Python',
+            'Notedly - A note taking app with React JS, Express, GraphQL',
+            'ConNET - A simple to do list project with Laravel',
+            'Movie - A simple movie UI with pure css. Just to show, I am professional in CSS',
+            'Learning App - A simple learning site UI with HTML, CSS, JS',
+            'AudiBook - UI Assignment from YHA Computer',
+            'Todo List - A simple todo list project with PHP',
+            'RealTime Chat - A simple chat app with PHP',
+            'Digital Hunter Challenge 2 - UI Project Challenge from DigitalHunter Course',
+            'Digital Hunter Challenge 5 - UI Project Challenge from DigitalHunter Course',
+            'Easy Bank Landing Page - Frontend Mentor Project Challenge',
+            'Rating Solution - UI Project Challenge from Frontend Mentor',
+            'beeMusic - Just to practice the JavaScript, Second Project',
+            'Music Player - Just to practice the JavaScript, First Project'
         ];
 
         $imagePath = public_path('image/encapsulation.jpg'); // Adjust the image path
